a69ec28ac3bfe51bca3dbf3c6bd0ffebaacb0a4b
Gentito <tsietsiramosedi6@gmail.com>
2022-07-01 17:22:29 -0700

    Print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0) Always (Success)
+ */
+int main(void)
+{
+char al = 'a';
+while (al <= 'z')
+{
+putchar(al);
+al++;
+}
+putchar('\n');
+return (0);
+}
