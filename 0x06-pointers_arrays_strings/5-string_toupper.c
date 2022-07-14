32f535e11f03d2da53bee7a2be0a43eaee7c5696
Gentito <tsietsiramosedi6@gmail.com>
2022-07-14 15:47:37 -0700

    Convert string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper - convert string to uppercase
+ * @s: pointer to string input
+ * Return: s
+ */
+char *string_toupper(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+}
+return (s);
+}
