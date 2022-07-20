57ea8d3e559bda21d1eb9a8913f928de86071f3e
Gentito <tsietsiramosedi6@gmail.com>
2022-07-20 10:07:09 -0700

    X raised to the power of Y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - x raised to the power of y
+ * @x: integer input
+ * @y: integer input
+ * Return: recursion
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
