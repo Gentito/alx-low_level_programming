061999b497fee78323b86039194ef60506ed5411
Gentito <tsietsiramosedi6@gmail.com>
2022-07-20 09:45:54 -0700

    Print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion -print string in reverse
+ * @s: pointer to character input
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
